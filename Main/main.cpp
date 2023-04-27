a93de70fbcfcd58e2c05948fd4c733f827e3dcf3
sstej88 <saitejsunkara9@gmail.com>
2023-04-27 13:15:13 -0400

    Main Function and IO updated

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,8 +1,56 @@
-#include <iostream>
+/*
+ * ACM ICPC preparation
+ * Author: Sai Tej Sunkara
+ * Topic: Main Default Function
+ * Sequence Containers: Arrays, Vectors, Deque, Forward List, List
+ * Associative Containers: Map, Set, Multimap, Multiset
+ * Unordered Associative Containers: Unordered Map, Unordered Set, Unordered Multimap, Unordered Multiset
+ * Container Adapters: Stack, Queue, Priority Queue
+*/
+
+#include<bits/stdc++.h>
+#include<ctime>
+#define int long long int
+#define double long double
+#define endl "\n"
+#define RUN_LOCAL
 using namespace std;
 
-int32_t main()
-{
-    cout << "Hello world" << endl;
-    return 0;
+inline int pow(int a, int b) {
+    int x = 1;
+    while(b) {
+        if(b&1) x*=a;
+        a*=a;
+        b>>=1;
+    }
+    return x;
+}
+
+vector<int> solve() {
+    vector<int> r;
+
+    r.push_back(10);
+    r.push_back(30);
+    r.push_back(40);
+    
+    return r;
+}
+
+int32_t main() {
+    clock_t initialTime = clock();
+    #ifdef RUN_LOCAL
+        freopen("../io/input.txt", "r", stdin);
+        freopen("../io/output.txt", "w", stdout);
+        freopen("../io/err.txt", "w", stderr);
+    #endif
+    int test = 1;
+    cin>>test;
+    while(test--) {
+        vector<int> result = solve();
+        for(auto x: result) {
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+    cerr<<((double)clock()-initialTime)/CLOCKS_PER_SEC<<endl;
 }
