96f5ad5f8ec9a9686fcd4032da0db2025f20b8dd
sstej88 <saitejsunkara9@gmail.com>
2023-05-12 17:20:43 -0400

    Modulo Binary Exponentiation Implemented

---
+++ Linear Recurrences and Matrix Exponentiation/ModularBinaryExponentiation.cpp
@@ -0,0 +1,78 @@
+/*
+ * ACM ICPC preparation
+ * Author: Sai Tej Sunkara
+ * Topic: 
+ * 
+ * Sequence Containers: Arrays, Vectors, Deque, Forward List, List
+ * Associative Containers: Map, Set, Multimap, Multiset
+ * Unordered Associative Containers: Unordered Map, Unordered Set, Unordered Multimap, Unordered Multiset
+ * Container Adapters: Stack, Queue, Priority Queue
+*/
+
+#include<bits/stdc++.h>
+#include<ctime>
+#define int long long int
+#define double long double
+#define endl "\n"
+#define RUN_LOCAL
+using namespace std;
+
+const int mod = 1e9+7;
+// Whenever you do a mod operation with this mod, the resultant value will be always less than 1e18+9 which is a maximum value a long long int can store.
+
+template<typename T>
+inline T pow(T a, T b) {
+    T x = 1;
+    while(b) {
+        if(b&1) x*=a; x = x%mod;
+        a*=a; a = a%mod;
+        b>>=1;
+    }
+    return x;
+}
+
+template<typename T>
+inline T isEven(T a) {
+    if(a&1) return false;
+    else return true;
+}
+
+template<typename T>
+inline T isTwoPower(T a) {
+    if((a&(a-1))==0) return true;
+    else return false;
+}
+
+template<typename T>
+T bug(T x) {
+    cout<<"Bug: "<<x<<endl;
+}
+
+vector<int> solve() {
+    vector<int> r;
+
+    int re = pow(2, 50);
+    r.push_back(re);
+
+    return r;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);cerr.tie(0);
+    clock_t initialTime = clock();
+    #ifdef RUN_LOCAL
+        freopen("../io/input.txt", "r", stdin);
+        freopen("../io/output.txt", "w", stdout);
+        freopen("../io/err.txt", "w", stderr);
+    #endif
+    int test = 1;
+    cin>>test;
+    while(test--) {
+        vector<int> result = solve();
+        for(auto x: result) {
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+    cerr<<((double)clock()-initialTime)/CLOCKS_PER_SEC<<endl;
+}
