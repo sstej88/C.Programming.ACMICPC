c2bcc783d1c5e82b6b2edfca5d9851782f4676b7
sstej88 <saitejsunkara9@gmail.com>
2023-05-12 13:22:40 -0400

    Power Function updated

---
+++ Problems/F95.cpp
@@ -0,0 +1,88 @@
+/*
+ * ACM ICPC preparation
+ * Author: Sai Tej Sunkara
+ * Topic: 
+ * 
+ * Sequence Containers: Arrays, Vectors, Deque, Forward List, List
+ * Associative Containers: Map, Set, Multimap, Multiset
+ * Unordered Associative Containers: Unordered Map, Unordered Set, Unordered Multimap, Unordered Multiset
+ * Container Adapters: Stack, Queue, Priority Queue
+*/
+
+#include<bits/stdc++.h>
+#include<ctime>
+#define int long long int
+#define double long double
+#define endl "\n"
+#define RUN_LOCAL
+using namespace std;
+
+template<typename T>
+inline T pow(T a, T b) {
+    T x = 1;
+    while(b) {
+        if(b&1) x*=a;
+        a*=a;
+        b>>=1;
+    }
+    return x;
+}
+
+template<typename T>
+inline T isEven(T a) {
+    if(a&1) return false;
+    else return true;
+}
+
+template<typename T>
+inline T isTwoPower(T a) {
+    if((a&(a-1))==0) return true;
+    else return false;
+}
+
+template<typename T>
+T bug(T x) {
+    cout<<"Bug: "<<x<<endl;
+}
+
+double myPow(double x, int n) {
+    if(n>=0) {
+        double r = 1;
+        while(n) {
+            if(n&1) r=r*x;
+            x=x*x;
+            n/=2;
+        }
+        return r;
+    }
+    else {
+        int p = n;
+        p = -p;
+        double r = 1;
+        while(p) {
+            if(p&1) r=r*x;
+            x=x*x;
+            p/=2;
+        }
+        return 1/r;
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);cerr.tie(0);
+    clock_t initialTime = clock();
+    #ifdef RUN_LOCAL
+        freopen("../io/input.txt", "r", stdin);
+        freopen("../io/output.txt", "w", stdout);
+        freopen("../io/err.txt", "w", stderr);
+    #endif
+    int test = 1;
+    cin>>test;
+    while(test--) {
+        double x;
+        int n;
+        cin>>x>>n;
+        cout<<myPow(x, n)<<endl;
+    }
+    cerr<<((double)clock()-initialTime)/CLOCKS_PER_SEC<<endl;
+}
