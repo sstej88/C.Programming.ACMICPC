d12601d4d403cbade0212b32fd0bd321987790d2
sstej88 <saitejsunkara9@gmail.com>
2023-05-14 16:14:57 -0400

    Fast Multiplication Implemented

---
+++ Linear Recurrences and Matrix Exponentiation Revision/FastMultiplication.cpp
@@ -0,0 +1,104 @@
+/*
+ * ACM ICPC preparation
+ * Author: Sai Tej Sunkara
+ * Topic: Fast Multiplication
+ * 
+ * Sequence Containers: Arrays, Vectors, Deque, Forward List, List
+ * Associative Containers: Map, Set, Multimap, Multiset
+ * Unordered Associative Containers: Unordered Map, Unordered Set, Unordered Multimap, Unordered Multiset
+ * Container Adapters: Stack, Queue, Priority Queue
+*/
+
+#include<bits/stdc++.h>
+#include<ctime>
+#define int long long int
+#define double long double
+#define endl "\n"
+#define RUN_LOCAL
+using namespace std;
+
+const int mod = 1e9+7;
+// Why are we maintaining every value less than 1e+7 although we have a space till 1e18? When we are multiplying 2 numbers of 1e9+7, then they can get stored in 1e18.
+
+template<typename D, typename I>
+inline D binaryExponentiation(D a, I b) {
+    D r = 1;
+    while(b) {
+        if(b&1) r*=a; r%=mod;
+        a*=a; a%=mod;
+        b = b>>1;
+    }
+    return r;
+}
+
+template<typename D, typename I>
+inline D powExponentiation(D a, I b) { // Binary Exponentiation
+    if(b>=0) {
+        return binaryExponentiation(a, b);
+    }
+    else {
+        b = -b;
+        D r = binaryExponentiation(a, b);
+        return 1/r;
+    }
+}
+
+template<typename T>
+inline T fastMultiplication(T a, T b) {
+    T r = 0;
+    b%=mod;
+    while(b) {
+        if(b&1) r+=a; r%=mod;
+        a+=a;
+        a%=mod;
+        b>>=1;
+    }
+    return r;
+}
+
+template<typename T>
+inline T isEven(T a) {
+    if(a&1) return false;
+    else return true;
+}
+
+template<typename T>
+inline T isTwoPower(T a) {
+    if((a&(a-1))==0) return true;
+    else return false;
+}
+
+template<typename T>
+T bug(T x) {
+    cout<<"Bug: "<<x<<endl;
+}
+
+vector<int> solve() {
+    vector<int> r;
+
+    int a, b;
+    cin>>a>>b;
+    cout<<fastMultiplication(a, b)<<endl;
+
+    return r;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);cerr.tie(0);
+    clock_t initialTime = clock();
+    #ifdef RUN_LOCAL
+        freopen("../io/input.txt", "r", stdin);
+        freopen("../io/output.txt", "w", stdout);
+        freopen("../io/err.txt", "w", stderr);
+    #endif
+    int test = 1;
+    cin>>test;
+    while(test--) {
+        vector<int> result = solve();
+        for(auto x: result) {
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+    cerr<<((double)clock()-initialTime)/CLOCKS_PER_SEC<<endl;
+}
