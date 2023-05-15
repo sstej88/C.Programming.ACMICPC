ff74c5f47aef6d84ae6857ee5271888de3ad1f17
sstej88 <saitejsunkara9@gmail.com>
2023-05-14 20:41:49 -0400

    FiboSum implemented

---
+++ Problems/F117.cpp
@@ -0,0 +1,189 @@
+/*
+ * ACM ICPC preparation
+ * Author: Sai Tej Sunkara
+ * Topic: FiboSum
+ * 
+ * Sequence Containers: Arrays, Vectors, Deque, Forward List, List
+ * Associative Containers: Map, Set, Multimap, Multiset
+ * Unordered Associative Containers: Unordered Map, Unordered Set, Unordered Multimap, Unordered Multiset
+ * Container Adapters: Stack, Queue, Priority Queue
+*/
+
+#include<bits/stdc++.h>
+#include<ctime>
+#define int long long int
+#define double long double
+#define endl "\n"
+#define RUN_LOCAL
+using namespace std;
+
+const int mod = 1e9+7;
+// Why are we maintaining every value less than 1e+7 although we have a space till 1e18? When we are multiplying 2 numbers of 1e9+7, then they can get stored in 1e18.
+
+template<typename D, typename I>
+inline D binaryExponentiation(D a, I b) {
+    D r = 1;
+    while(b) {
+        if(b&1) r*=a; r%=mod;
+        a*=a; a%=mod;
+        b = b>>1;
+    }
+    return r;
+}
+
+template<typename D, typename I>
+inline D powExponentiation(D a, I b) { // Binary Exponentiation
+    if(b>=0) {
+        return binaryExponentiation(a, b);
+    }
+    else {
+        b = -b;
+        D r = binaryExponentiation(a, b);
+        return 1/r;
+    }
+}
+
+template<typename T>
+inline T fastMultiplication(T a, T b) {
+    T r = 0;
+    b%=mod;
+    while(b) {
+        if(b&1) r+=a; r%=mod;
+        a+=a;
+        a%=mod;
+        b>>=1;
+    }
+    return r;
+}
+
+template<typename T>
+inline T isEven(T a) {
+    if(a&1) return false;
+    else return true;
+}
+
+template<typename T>
+inline T isTwoPower(T a) {
+    if((a&(a-1))==0) return true;
+    else return false;
+}
+
+template<typename T>
+T bug(T x) {
+    cout<<"Bug: "<<x<<endl;
+}
+
+const int matSize = 2;
+struct Mat {
+    int mat[matSize][matSize];
+    Mat() {
+        for(int i=0; i<matSize; i++) {
+            for(int j=0; j<matSize; j++) {
+                mat[i][j] = 0;
+            }
+        }
+    }
+    void identity() {
+        Mat();
+        for(int i=0; i<matSize; i++) {
+            mat[i][i] = 1;
+        }
+    }
+    void display() {
+        cout<<"Displaying Matrix: "<<endl;
+        for(int i=0; i<matSize; i++) {
+            for(int j=0; j<matSize; j++) {
+                cout<<mat[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<endl;
+    }
+
+    Mat operator* (Mat a) {
+        Mat result;
+        for(int i=0; i<matSize; i++) {
+            for(int j=0; j<matSize; j++) {
+                for(int k=0; k<matSize; k++) {
+                    result.mat[i][j] = result.mat[i][j] + mat[i][k]*a.mat[k][j];
+                    result.mat[i][j]%=mod;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+int fibonacci(int n) {
+    // Here we don't need F(n) and F(n+1) matrices. We just need them on paper. We will work on powers on T and then by intution, we get the values of F(n) by multiplying by [[1], [0]] 
+    Mat r;
+    Mat a;
+    
+    r.identity();
+    for(int i=0; i<matSize; i++) {
+        for(int j=0; j<matSize; j++) {
+            int e;
+            cin>>e;
+            a.mat[i][j] = e;
+        }
+    }
+
+    while(n) {
+        if(n&1) {
+            r = r*a;
+        }
+        a = a*a;
+        n>>=1;
+    }
+
+    return (r.mat[0][0]+r.mat[0][1])%mod;
+}
+
+vector<int> solve() {
+    vector<int> r;
+
+    int n, m;
+    cin>>n>>m;
+    vector<int> fib;
+    fib.push_back(0);
+    fib.push_back(1);
+    for(int i=2; i<=m; i++) {
+        fib.push_back(fib.at(i-1)+fib.at(i-2));
+    }
+    
+    vector<int> fibSum;
+    fibSum.push_back(0);
+    for(int i=1; i<=m; i++) {
+        fibSum.push_back(fib.at(i)+fibSum.at(i-1));
+    }
+    cout<<endl;
+    if(n>=1) {
+        r.push_back(fibSum.at(m)-fibSum.at(n-1));
+    }
+    else {
+        r.push_back(fibSum.at(m)-fibSum.at(n));
+    }
+    
+
+    return r;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);cerr.tie(0);
+    clock_t initialTime = clock();
+    #ifdef RUN_LOCAL
+        freopen("../io/input.txt", "r", stdin);
+        freopen("../io/output.txt", "w", stdout);
+        freopen("../io/err.txt", "w", stderr);
+    #endif
+    int test = 1;
+    cin>>test;
+    while(test--) {
+        vector<int> result = solve();
+        for(auto x: result) {
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+    cerr<<((double)clock()-initialTime)/CLOCKS_PER_SEC<<endl;
+}
