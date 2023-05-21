a8808ed4c8e37b4dd0653a3cca665c7f244471b9
sstej88 <saitejsunkara9@gmail.com>
2023-05-21 16:20:25 -0400

    Count Primes Executed - Sieve's Prime Number Theorem of Eratosthenes

---
+++ Problems/F159.cpp
@@ -0,0 +1,199 @@
+/*
+ * ACM ICPC preparation
+ * Author: Sai Tej Sunkara
+ * Topic: 
+ * 
+ * Sequence Containers: Arrays, Vectors, Deque, Forward List, List
+ * Associative Containers: Map, Set, Multimap, Multiset
+ * Unordered Associative Containers: Unordered Map, Unordered Set, Unordered Multimap, Unordered Multiset
+ * Container Adapters: Stack, Queue, Priority Queue
+*/
+
+#include<bits/stdc++.h>
+#include<ctime>
+#define int long long int
+#define double long double
+#define endl "\n"
+#define RUN_LOCAL
+using namespace std;
+
+const int mod = 1e9+7;
+// Why are we maintaining every value less than 1e+7 although we have a space till 1e18? When we are multiplying 2 numbers of 1e9+7, then they can get stored in 1e18.
+const int N = 1000000;
+
+template<typename D, typename I>
+inline D binaryExponentiation(D a, I b) {
+    D r = 1;
+    while(b) {
+        if(b&1) r*=a; r%=mod;
+        a*=a; a%=mod;
+        b = b>>1;
+    }
+    return r;
+}
+
+template<typename D, typename I>
+inline D powExponentiation(D a, I b) { // Binary Exponentiation
+    if(b>=0) {
+        return binaryExponentiation(a, b);
+    }
+    else {
+        b = -b;
+        D r = binaryExponentiation(a, b);
+        return 1/r;
+    }
+}
+
+template<typename T>
+vector<bool> primeSieve(T N) {
+    vector<bool> sieve(N, true);
+
+    sieve[0] = false;
+    sieve[1] = false;
+
+    for(T i=2; i<=N; i++) {
+        if(sieve[i]) {
+            for(T j=i*i; j<=N; j=j+i) {
+                sieve[j] = false;
+            }
+        }
+        
+    }
+
+    return sieve;
+}
+
+template<typename T>
+inline T fastMultiplication(T a, T b) {
+    T r = 0;
+    b%=mod;
+    while(b) {
+        if(b&1) r+=a; r%=mod;
+        a+=a;
+        a%=mod;
+        b>>=1;
+    }
+    return r;
+}
+
+template<typename T>
+inline T isEven(T a) {
+    if(a&1) return false;
+    else return true;
+}
+
+template<typename T>
+inline T isTwoPower(T a) {
+    if((a&(a-1))==0) return true;
+    else return false;
+}
+
+template<typename T>
+T bug(T x) {
+    cout<<"Bug: "<<x<<endl;
+}
+
+const int matSize = 2;
+struct Mat {
+    int mat[matSize][matSize];
+    Mat() {
+        for(int i=0; i<matSize; i++) {
+            for(int j=0; j<matSize; j++) {
+                mat[i][j] = 0;
+            }
+        }
+    }
+    void identity() {
+        Mat();
+        for(int i=0; i<matSize; i++) {
+            mat[i][i] = 1;
+        }
+    }
+    void display() {
+        cout<<"Displaying Matrix: "<<endl;
+        for(int i=0; i<matSize; i++) {
+            for(int j=0; j<matSize; j++) {
+                cout<<mat[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<endl;
+    }
+
+    Mat operator* (Mat a) {
+        Mat result;
+        for(int i=0; i<matSize; i++) {
+            for(int j=0; j<matSize; j++) {
+                for(int k=0; k<matSize; k++) {
+                    result.mat[i][j] = result.mat[i][j] + mat[i][k]*a.mat[k][j];
+                    result.mat[i][j]%=mod;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+int fibonacci(int n) {
+    // Here we don't need F(n) and F(n+1) matrices. We just need them on paper. We will work on powers on T and then by intution, we get the values of F(n) by multiplying by [[1], [0]] 
+    Mat r;
+    Mat a;
+    
+    r.identity();
+    for(int i=0; i<matSize; i++) {
+        for(int j=0; j<matSize; j++) {
+            int e;
+            cin>>e;
+            a.mat[i][j] = e;
+        }
+    }
+
+    while(n) {
+        if(n&1) {
+            r = r*a;
+        }
+        a = a*a;
+        n>>=1;
+    }
+
+    return (r.mat[0][0]+r.mat[0][1])%mod;
+}
+
+int countPrimes(int n) {
+    if(n>2) {
+        int count = 0;
+        vector<int> primes(n, 1);
+        primes[0] = primes[1] = 0;
+        for(int i=2; i<n; i++) {
+            if(primes[i]==1) {
+                count++;
+                for(int j=i*i; j<n; j+=i) {
+                    primes[j] = 0;
+                }
+            }
+        }
+        return count;
+    }
+    else {
+        return 0;
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);cerr.tie(0);
+    clock_t initialTime = clock();
+    #ifdef RUN_LOCAL
+        freopen("../io/input.txt", "r", stdin);
+        freopen("../io/output.txt", "w", stdout);
+        freopen("../io/err.txt", "w", stderr);
+    #endif
+    int test = 1;
+    cin>>test;
+    while(test--) {
+        int n;
+        cin>>n;
+        cout<<countPrimes(n)<<endl;
+        cout<<endl;
+    }
+    cerr<<((double)clock()-initialTime)/CLOCKS_PER_SEC<<endl;
+}
