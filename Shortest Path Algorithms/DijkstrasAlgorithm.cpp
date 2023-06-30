0506c94b3ed3a28d03dde8735e0b8ab1d5c24133
sstej88 <saitejsunkara9@gmail.com>
2023-06-30 06:37:34 -0400

    Test Case Dijkstra's Algorithm

--- Shortest Path Algorithms/DijkstrasAlgorithm.cpp
+++ Shortest Path Algorithms/DijkstrasAlgorithm.cpp
@@ -582,7 +582,7 @@ class SpGraph {
 vector<int> solve() {
     vector<int> nums;
 
-    SpGraph g(5, 0);
+    SpGraph g(5, 1);
     g.addEdge(0, 1, 1);
     g.addEdge(0, 2, 4);
     g.addEdge(0, 3, 7);
@@ -590,7 +590,7 @@ vector<int> solve() {
     g.addEdge(2, 3, 2);
     g.addEdge(3, 4, 3);
 
-    g.dijkstraAlgorithm(0, 0);
+    g.dijkstraAlgorithm(1, 0);
     for(int i=0; i<g.numberOfNodes; i++) {
         cout<<i<<" - "<<g.result[i]<<endl;
     }
